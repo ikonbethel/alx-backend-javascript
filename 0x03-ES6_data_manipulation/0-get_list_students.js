fe06fd1f0bcdf3dc92daf86483fa759e09abfc52
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-10-08 14:04:26 -0700

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
-	const objOne = {
-		id: 1,
-    	firstName: 'Guillaume',
-    	location: 'San Francisco',
+  const objOne = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
   };
-  	const objTwo = {
-    	id: 2,
-    	firstName: 'James',
-    	location: 'Columbia',
+  const objTwo = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
   };
-  	const objThree = {
-    	id: 5,
-    	firstName: 'Serena',
-    	location: 'San Francisco',
+  const objThree = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
   };
 
-  	return [objOne, objTwo, objThree];
+  return [objOne, objTwo, objThree];
 }
