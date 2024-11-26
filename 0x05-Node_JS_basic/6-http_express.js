af0ea80af09ad6bc97b9aeb6b2dd1ea10c72b0af
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-11-26 03:27:09 +0100

    Create 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// Simple express app
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Started server at http://localhost:${port}`);
+});
+
+module.exports = app;
