fd3cfe9e827c77a73be04eec58b87a2b60ac021a
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-11-29 04:16:55 +0100

    Create api.js

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,14 @@
+// api.js
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
